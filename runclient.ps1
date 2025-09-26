3f41f7735c7ac2335b66ef8b42e9f0aeef0f2297
Paweł <pawel.wojcik080@gmail.com>
2025-09-26 16:29:58 +0200

    Update runclient.ps1

--- runclient.ps1
+++ runclient.ps1
@@ -1,5 +1,8 @@
 # runclient.ps1
 
+# Pobierz zależności
+pip install -e ".[ai]"
+
 # 1. Otwórz tunel SSH na porcie 8000
 Start-Process powershell -ArgumentList '-NoExit', '-Command', 'ssh -NL localhost:8000:localhost:8000 s490115@g1n2.cluster.wmi.amu.edu.pl'
 
@@ -8,4 +11,5 @@ Start-Sleep -Seconds 3
 
 # 3. Odpal klienta
 $env:PYTHONPATH="src"
+
 python client_tk.py --host localhost --port 8000
