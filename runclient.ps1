0ff7df9c7fe5b6e5bf2a9b3267dea47a9d7fb609
Paweł <pawel.wojcik080@gmail.com>
2025-09-26 16:39:59 +0200

    change runclient.ps1 to a specific adress (deployed)

--- runclient.ps1
+++ runclient.ps1
@@ -3,13 +3,7 @@
 # Pobierz zależności
 pip install -e ".[ai]"
 
-# 1. Otwórz tunel SSH na porcie 8000
-Start-Process powershell -ArgumentList '-NoExit', '-Command', 'ssh -NL localhost:8000:localhost:8000 s490115@g1n2.cluster.wmi.amu.edu.pl'
-
-# 2. Poczekaj chwilę, aż tunel się zestawi
-Start-Sleep -Seconds 3
-
 # 3. Odpal klienta
 $env:PYTHONPATH="src"
+python client_tk.py --host 10.71.81.2 --port 8000
 
-python client_tk.py --host localhost --port 8000
